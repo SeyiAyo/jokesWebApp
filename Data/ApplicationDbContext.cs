5510cfd3a4b3b2f4a843c9816c497884ac79c768
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2024-03-27 01:36:42 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using jokesWebApp.Models;
+
+namespace jokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<jokesWebApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
